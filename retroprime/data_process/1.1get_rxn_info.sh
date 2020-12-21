c7b2b0a31de75970bfd40c269a29039fa17be75d
wangxr0526 <59350191+wangxr0526@users.noreply.github.com>
2020-12-21 19:57:45 +0800

    Delete 1.1get_rxn_info.sh

--- retroprime/data_process/1.1get_rxn_info.sh
+++
@@ -1,3 +0,0 @@
-clean_data=../../databox/uspto_full/single/new_raw_all.csv
-output_dir=../../databox/uspto_full/single/index_save/
-python check_rxn.py -clean_data $clean_data -output_dir $output_dir
